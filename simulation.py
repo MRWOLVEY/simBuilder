63e7eeefb0378dca6d16cae89b090dd7439a2690
Wolvey <wolfeyplays123@gmail.com>
2023-04-08 22:14:47 +0200

    fixed variable names

--- simulation.py
+++ simulation.py
@@ -7,22 +7,21 @@ class digitTable:
         self.p=1
         self.range=dict()
 
-    def cumP(self,props):
+    def cummulative_prop(self,props):
         summ=0
         output=dict()
         for k,v in props.items():
             summ+=v
             output[k]=summ
         return output
-    # self.cum=cumP(self.dist)
     
     
 
     def prop_range(self, p=1):
         i = 1
-        cum=self.cumP(self.dist)
+        cp=self.cummulative_prop(self.dist)
         output = dict()
-        for k, v in cum.items():
+        for k, v in cp.items():
             o = v * (10 ** p)
             output[k] = range(i, int(o)+1)
             i = int(o) + 1
@@ -36,8 +35,8 @@ st=digitTable({1:0.35,3:0.55,5:0.10})
 # print(iat.prop_range())
 
 class simulationTable:
-    def __init__(self,cust,iat,p1,st,p2):
-        self.cust=cust
+    def __init__(self,custNo,iat,p1,st,p2):
+        self.custNo=custNo
         self.iat=iat
         self.st=st
         self.p1=p1
@@ -45,11 +44,11 @@ class simulationTable:
         iat.prop_range()
         st.prop_range(2)
     
-    def iat_ass(self):
+    def iat_assign(self):
         output=dict()
         error=dict()
         output[1]=0
-        for i in range(2,self.cust+1):
+        for i in range(2,self.custNo+1):
             tmp=random.randint(1,10**self.p1)
             if tmp == 10**self.p1:
                 tmp=0
@@ -60,9 +59,9 @@ class simulationTable:
                     error[i]=['rnd=',tmp,'key=',k,'value=',v]
         # print(error)
         return output
-    def st_ass(self):
+    def st_assign(self):
         output=dict()
-        for i in range(1,self.cust+1):
+        for i in range(1,self.custNo+1):
             tmp=random.randint(1,10**self.p2)
             if tmp == 10**self.p2:
                 tmp=0
@@ -74,13 +73,13 @@ class simulationTable:
     def calc_arrival_time(self):
         p=0
         output=dict()
-        for i in range(1,self.cust+1):
-            output[i]=self.iat_ass()[i]+p
+        for i in range(1,self.custNo+1):
+            output[i]=self.iat_assign()[i]+p
             print(output[i])
             p=output[i]
         return output
 
 
-stAss=simulationTable(5,iat,1,st,2)
-print(stAss.iat_ass(),'\n',stAss.st_ass(),'\n',end="")
-# print(stAss.calc_arrival_time())
+simTable=simulationTable(5,iat,1,st,2)
+print(simTable.iat_assign(),'\n',simTable.st_assign(),'\n',end="")
+# print(simTable.calc_arrival_time())
