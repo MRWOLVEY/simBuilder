47ae85dba7f051e8759091a44920239a482d08a3
Wolvey <wolfeyplays123@gmail.com>
2023-04-12 01:19:11 +0200

    fixed the damn bug

--- simulation.py
+++ simulation.py
@@ -24,16 +24,17 @@ class digitTable:
         output = dict()
         for k, v in cp.items():
             o = v * (10 ** p)
+            if o == 10 ** p:
+                o-=1
             output[k] = range(i, int(o)+1)
             i = int(o) + 1
         self.p=p
         self.range=output
-        # return output
+        return output
     
 
 iat=digitTable({1:0.5,3:0.3,6:0.2})
 st=digitTable({1:0.35,3:0.55,5:0.10})
-# print(iat.prob_range())
 
 class simulationTable:
     def __init__(self,custNo,iat,p1,st,p2):
@@ -47,40 +48,39 @@ class simulationTable:
     
     def iat_assign(self):
         output=dict()
-        error=dict()
         output[1]=0
         for i in range(2,self.custNo+1):
-            tmp=random.randint(1,10**self.p1)
-            if tmp == 10**self.p1:
-                tmp=0
+            tmp=random.randint(0,10**self.p1-1)
+            if tmp == 0:
+                tmp=10**self.p1-1            
             for k,v in self.iat.range.items():
                 if tmp in v:
                     output[i]=k
-                else:
-                    error[i]=['rnd=',tmp,'key=',k,'value=',v]
-        # print(error)
         return output
     def st_assign(self):
         output=dict()
         for i in range(1,self.custNo+1):
-            tmp=random.randint(1,10**self.p2)
-            if tmp == 10**self.p2:
-                tmp=0
+            tmp=random.randint(0,10**self.p2-1)
+            if tmp == 0:
+                tmp=10**self.p2-1
             for k,v in self.st.range.items():
                 if tmp in v:
                     output[i]=k
         return output
     
+    
     def calc_arrival_time(self):
+        iat=self.iat_assign()
         p=0
         output=dict()
         for i in range(1,self.custNo+1):
-            output[i]=self.iat_assign()[i]+p
-            print(output[i])
+            output[i]=iat[i]+p
             p=output[i]
         return output
 
-
-simTable=simulationTable(5,iat,1,st,2)
-print(simTable.iat_assign(),'\n',simTable.st_assign(),'\n',end="")
+ #debugging:
+# simTable=simulationTable(5,iat,1,st,2)
+# tmp=digitTable({1:0.5,3:0.3,6:0.2})
+# print(tmp.prob_range())
+# print(simTable.iat_assign(),'\n',simTable.st_assign(),'\n',end="")
 # print(simTable.calc_arrival_time())
